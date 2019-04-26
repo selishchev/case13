13c4629c89593ec0ff0f5613bf03d920be38510d
selishchev <selishev2010@gmail.com>
2019-04-26 16:40:21 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-lst_of_rooms = []
-lst_of_requests = [1, 2]
+from case13 import *
 
+lst_of_rooms = []
+lst_of_requests = []
+
+booking = open('booking.txt', 'r', encoding='utf-8')
+v = booking.readlines()
+for line in v:
+    line = line.split(' ')
+    fio_demo = [line[1], line[2], line[3]]
+    fio = ' '.join(fio_demo)
+    lst_of_requests.append(Request(line[0], fio, line[4], line[5], line[6], line[7]))
+booking.close()
+
+for i in lst_of_requests:
+    print(i)
+
+fund = open('fund.txt', 'r', encoding='utf-8')
+m = fund.readlines()
+for line in m:
+    line = line.split(' ')
+    lst_of_rooms.append(Room(line[0], line[1], line[2], line[3]))
+fund.close()
+
+for j in lst_of_rooms:
+    print(j)
 
 def customer_consent():
     pass
